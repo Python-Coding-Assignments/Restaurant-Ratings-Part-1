cc56c3cf9465620fdb9e27b3eb9afff6526330c4
Garrett Ellis <154717520+garrettbovo@users.noreply.github.com>
2024-01-21 19:34:13 -0500

    Delete Functions directory

--- Functions/menu.py
+++
@@ -1,9 +0,0 @@
-def menu():
-    """This function prints the following menu whenever the function is called."""
-
-    #printing menu to the screen
-    print("Please select from the following options...")
-    print(" A  - Display Restaurants")
-    print(" B  - Display all ratings for a restaurant")
-    print(" C  - Add a rating to a restaurant")
-    print(" Q  - Quit") 
